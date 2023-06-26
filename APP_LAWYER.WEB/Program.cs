744a9d96e92effa9c1da5137f6bab67ddbd2a9a7
torekhanmukhtarov <nahkerot@gmail.com>
2023-06-26 15:26:09 +0500

    added watsapp

--- APP_LAWYER.WEB/Program.cs
+++ APP_LAWYER.WEB/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext")));
 
@@ -30,6 +32,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthentication();
