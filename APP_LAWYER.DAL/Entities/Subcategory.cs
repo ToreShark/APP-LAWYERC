7e7f6c787b22ec193e96a40a726119df5125388c
torekhanmukhtarov <nahkerot@gmail.com>
2023-07-11 20:46:02 +0500

    added correct Radik

--- APP_LAWYER.DAL/Entities/Subcategory.cs
+++ APP_LAWYER.DAL/Entities/Subcategory.cs
@@ -5,7 +5,8 @@ public class Subcategory : BaseEntity
     public string Name { get; set; }
     public string Description { get; set; }
     public string FullDescription { get; set; }
-    public string Image { get; set; }
+    public string? Image { get; set; }
+    public string? ImagePath { get; set; }
     public string Content { get; set; }
     public virtual Category? Category { get; set; }
     public string Slug { get; set; }
